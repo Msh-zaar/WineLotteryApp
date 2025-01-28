790b468160cd431ba1a961a51199d1cfe85728d8
Michael Zaar <michael.zaar@OBOS.NO>
2025-01-28 21:26:45 +0100

    added test frontend

--- WineLotteryApp.Domain/Models/Ticket.cs
+++ WineLotteryApp.Domain/Models/Ticket.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WineLotteryApp.Domain.Models;
+﻿namespace WineLotteryApp.Domain.Models;
 
 public class Ticket
 {
